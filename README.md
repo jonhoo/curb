6c43cb6b22825467c19f6c6325af44dd2a9eece3
Jon Gjengset <jon@thesquareplanet.com>
2019-03-15 17:07:50 -0400

    First attempt

---
+++ README.md
@@ -0,0 +1,58 @@
+A command-line tool to restrict a process from using particular hardware
+resources. It is particularly geared towards performance-sensitive
+multi-core experiments, in which you may want to avoid or measure the
+impact of CPU features like [SMT] (e.g., [Intel Hyper-Threading] or
+[NUMA memory]. While this is possible with existing tools like
+[`hwloc-bind`] and [`numactl`], it's often pretty inconvenient to get
+the result you want. With `curb` on the other hand:
+
+```console
+$ curb --no-smt --no-numa mycommand
+```
+
+Will run `mycommand` _only_ on each physical core on a single NUMA node
+(the first one specifically). Note that if you want to pass additional
+arguments to the command under test, you can do so using `--`:
+
+```console
+$ curb --no-smt mycommand -- --no-crash --performance better
+```
+
+To visualize what CPUs are being used using [`lstopo`], do:
+
+```console
+$ curb --no-smt --no-numa lstopo -- --pid 0
+```
+
+That should show all active cores in green. If you don't have a window
+manager running, add `-v` at the end and look for PUs marked with
+`(running)`.
+
+## Dependencies
+
+Curb depends on the [`hwloc` crate], which in turn depends on the
+[`hwloc` C library].
+
+## OS Support
+
+Curb currently only works on `cfg(unix)` systems, because it sets the
+CPU binding [for the current process], and then calls [`exec`]. This
+should be possible to work around by instead spawning the process,
+getting its identifier, calling [`set_cpubind_for_process`], and then
+waiting for the process to exit. However, this has a number of
+downsides, like the process _initially_ being unconstrained and having
+to proxy things like stdin, stdout, signals, and exit codes. Suggestions
+for how to support non-UNIX platforms are warmly welcome!
+
+
+  [SMT]: https://en.wikipedia.org/wiki/Simultaneous_multithreading
+  [Intel Hyper-Threading]: https://en.wikipedia.org/wiki/Hyper-threading
+  [NUMA memory]: https://en.wikipedia.org/wiki/Non-uniform_memory_access
+  [`hwloc-bind`]: https://linux.die.net/man/1/hwloc-bind
+  [`numactl`]: https://github.com/numactl/numactl
+  [`lstopo`]: https://linux.die.net/man/1/lstopo
+  [`hwloc` crate]: https://github.com/daschl/hwloc-rs
+  [`hwloc` C library]: https://github.com/daschl/hwloc-rs#prerequisites
+  [for the current process]: https://docs.rs/hwloc/0.5.0/hwloc/struct.Topology.html#method.set_cpubind
+  [`exec`]: https://doc.rust-lang.org/std/os/unix/process/trait.CommandExt.html#tymethod.exec
+  [`set_cpubind_for_process`]: https://docs.rs/hwloc/0.5.0/hwloc/struct.Topology.html#method.set_cpubind_for_process
---
+++ src/main.rs
@@ -0,0 +1,110 @@
+use std::io;
+use std::os::unix::process::CommandExt;
+use std::path::PathBuf;
+use std::process::{exit, Command};
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+#[structopt(author = "")]
+struct Opt {
+    /// Enable verbose debug output.
+    #[structopt(short = "v", long = "verbose")]
+    verbose: bool,
+
+    /// Disable simultaneous multithreading (SMT) technologies like Intel Hyper-Threading.
+    #[structopt(long = "no-smt")]
+    disable_smt: bool,
+
+    /// Disable NUMA by forcing the command to run on only a single numa node.
+    #[structopt(long = "no-numa")]
+    disable_numa: bool,
+
+    /// The command to run.
+    #[structopt(parse(from_os_str))]
+    command: PathBuf,
+
+    /// Argument to pass to the given command.
+    ///
+    /// You can use `--` in the argument list to pass arguments that would otherwise be interpreted
+    /// by curb. For example, if you want to pass `-v` to `command`, use `curb mybin -- -v`.
+    arguments: Vec<String>,
+}
+
+fn main() {
+    let opt = Opt::from_args();
+    println!("{:?}", opt);
+
+    let mut cmd = Command::new(&opt.command);
+    cmd.args(&opt.arguments);
+
+    // Before we exec, we need to set the right binds
+    let flags = hwloc::CPUBIND_PROCESS | hwloc::CPUBIND_STRICT;
+    let mut topo = hwloc::Topology::new();
+    let mut allowed = topo.get_cpubind(flags).unwrap();
+
+    if opt.disable_smt {
+        // clear bits for all but the first PU on each core
+        for pu in topo.objects_with_type(&hwloc::ObjectType::PU).unwrap() {
+            if opt.verbose {
+                eprintln!(
+                    "found PU#{} (sibling #{})",
+                    pu.os_index(),
+                    pu.sibling_rank()
+                );
+            }
+
+            if pu.parent().unwrap().object_type() == hwloc::ObjectType::Core
+                && pu.sibling_rank() != 0
+            {
+                if opt.verbose {
+                    eprintln!("disabling SMT PU");
+                }
+                allowed.unset(pu.os_index());
+            }
+        }
+    }
+
+    if opt.disable_numa {
+        for numa in topo
+            .objects_with_type(&hwloc::ObjectType::NUMANode)
+            .unwrap()
+            .into_iter()
+            .skip(1)
+        {
+            if opt.verbose {
+                eprintln!("found extra NUMA node #{}", numa.sibling_rank());
+            }
+
+            for cpu in numa.cpuset().unwrap() {
+                if opt.verbose {
+                    eprintln!("disabling extra NUMA PU");
+                }
+                allowed.unset(cpu);
+            }
+        }
+    }
+
+    if let Err(e) = topo.set_cpubind(allowed, flags) {
+        match e {
+            hwloc::CpuBindError::Generic(code, msg) => {
+                eprintln!("Could not bind hardware resources: {} ({})", msg, code);
+                exit(1);
+            }
+        }
+    }
+
+    let e = cmd.exec();
+    if e.kind() == io::ErrorKind::NotFound {
+        eprintln!("Unknown command {}", opt.command.display());
+        exit(127);
+    } else if e.kind() == io::ErrorKind::PermissionDenied {
+        eprintln!(
+            "The file '{}' is not executable by this user",
+            opt.command.display()
+        );
+        exit(126);
+    } else {
+        eprintln!("Could not execute {}: {}", opt.command.display(), e);
+        exit(125);
+    }
+}
